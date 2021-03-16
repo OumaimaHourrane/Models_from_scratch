1d8760bf552a991bf65481779b9b7947c92beffc
Oumaima Hourrane <oumaima.hourrane@gmail.com>
2021-03-16 12:42:47 +0100

    Add files via upload

---
+++ Pytorch/GPT/tokenizer.py
@@ -0,0 +1,104 @@
+from typing import List
+from collections import OrderedDict
+
+from prenlp.tokenizer import SentencePiece
+
+class Tokenizer:
+    def __init__(self, tokenizer, vocab_file: str,
+                 pad_token: str = '[PAD]',
+                 unk_token: str = '[UNK]',
+                 bos_token: str = '[BOS]',
+                 eos_token: str = '[EOS]',
+                 sep_token: str = '[SEP]',
+                 cls_token: str = '[CLS]',
+                 mask_token: str = '[MASK]'):
+        self.tokenizer = tokenizer
+        self.pad_token = pad_token
+        self.unk_token = unk_token
+        self.bos_token = bos_token
+        self.eos_token = eos_token
+        self.sep_token = sep_token
+        self.cls_token = cls_token
+        self.mask_token = mask_token
+        self.vocab = OrderedDict()
+        self.ids_to_tokens = OrderedDict()
+
+        # Build vocab and ids_to_tokens
+        with open(vocab_file, 'r', encoding='utf-8') as reader:
+            for i, line in enumerate(reader.readlines()):
+                token = line.split()[0]
+                self.vocab[token] = i
+        for token, id in self.vocab.items():
+            self.ids_to_tokens[id] = token
+
+    def tokenize(self, text: str) -> List[str]:
+        return self.tokenizer(text)
+
+    def convert_token_to_id(self, token: str) -> int:
+        return self.vocab.get(token, self.vocab.get(self.unk_token))
+
+    def convert_id_to_token(self, id: int) -> str:
+        return self.ids_to_tokens.get(id, self.unk_token)
+
+    def convert_tokens_to_ids(self, tokens: List[str]) -> List[int]:
+        return [self.convert_token_to_id(token) for token in tokens]
+
+    def convert_ids_to_tokens(self, ids: List[int]) -> List[str]:
+        return [self.convert_id_to_token(id) for id in ids]
+
+    @property
+    def vocab_size(self) -> int:
+
+        return len(self.vocab)
+
+    @property
+    def pad_token_id(self) -> int:
+
+        return self.convert_token_to_id(self.pad_token)
+
+    @property
+    def unk_token_id(self) -> int:
+
+        return self.convert_token_to_id(self.unk_token)
+
+    @property
+    def bos_token_id(self) -> int:
+
+        return self.convert_token_to_id(self.bos_token)
+
+    @property
+    def eos_token_id(self) -> int:
+
+        return self.convert_token_to_id(self.eos_token)
+
+    @property
+    def sep_token_id(self) -> int:
+
+        return self.convert_token_to_id(self.sep_token)
+
+    @property
+    def cls_token_id(self) -> int:
+
+        return self.convert_token_to_id(self.cls_token)
+
+    @property
+    def mask_token_id(self) -> int:
+
+        return self.convert_token_to_id(self.mask_token)
+
+class PretrainedTokenizer(Tokenizer):
+    def __init__(self, pretrained_model: str, vocab_file: str,
+                 pad_token: str = '[PAD]',
+                 unk_token: str = '[UNK]',
+                 bos_token: str = '[BOS]',
+                 eos_token: str = '[EOS]',
+                 sep_token: str = '[SEP]',
+                 cls_token: str = '[CLS]',
+                 mask_token: str = '[MASK]'):
+        tokenizer = SentencePiece.load(pretrained_model)
+
+        super(PretrainedTokenizer, self).__init__(tokenizer, vocab_file, pad_token, unk_token, bos_token, eos_token)
+
+    def detokenize(self, tokens: List[str]) -> str:
+
+        return self.tokenizer.detokenize(tokens)
